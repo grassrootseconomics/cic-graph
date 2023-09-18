8f11bbf6ec661e86394f3879f15a403a99907650
Mohamed Sohail <sohailsameja@gmail.com>
2023-09-18 18:14:32 +0800

    updates: vouchers table and add new commodity table (#31)
    * vouchers table: add new columns
    
    * see migration file
    
    * commodity: add new table to describe service/product listings

---
+++ migrations/cic_graph/1694419664787_voucher_updates/down.sql
@@ -0,0 +1,19 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TABLE IF NOT EXISTS voucher_type (
+--   value TEXT PRIMARY KEY
+-- );
+-- INSERT INTO voucher_type (value) VALUES
+-- ('DEMURRAGE'),
+-- ('GIFTABLE');
+--
+-- ALTER TABLE vouchers
+-- DROP COLUMN supply,
+-- DROP COLUMN demurrage_rate;
+--
+-- ALTER TABLE vouchers
+-- ADD COLUMN voucher_email TEXT,
+-- ADD COLUMN voucher_website TEXT,
+-- ADD COLUMN voucher_uoa TEXT NOT NULL DEFAULT 'KES',
+-- ADD COLUMN voucher_value INT NOT NULL DEFAULT 10,
+-- ADD COLUMN voucher_type TEXT REFERENCES voucher_type(value) NOT NULL DEFAULT 'DEMURRAGE';
