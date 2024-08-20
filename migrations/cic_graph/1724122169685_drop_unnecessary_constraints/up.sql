ec1b912081e4a9a05d5653b9b58ad549e8242a1b
Mohamed Sohail 天明 <sohailsameja@gmail.com>
2024-08-20 10:52:27 +0800

    fix: drop unnecessary db constraints (#38)
    * closes #37

---
+++ migrations/cic_graph/1724122169685_drop_unnecessary_constraints/up.sql
@@ -0,0 +1,17 @@
+ALTER TABLE personal_information DROP CONSTRAINT personal_information_family_name_check RESTRICT;
+ALTER TABLE personal_information DROP CONSTRAINT personal_information_year_of_birth_check RESTRICT;
+ALTER TABLE personal_information DROP CONSTRAINT personal_information_given_names_check RESTRICT;
+ALTER TABLE personal_information DROP CONSTRAINT personal_information_location_name_check RESTRICT;
+
+ALTER TABLE users DROP CONSTRAINT users_interface_identifier_check RESTRICT;
+
+ALTER TABLE voucher_certifications DROP CONSTRAINT voucher_certifications_certifier_weight_check RESTRICT;
+ALTER TABLE voucher_certifications DROP CONSTRAINT voucher_certifications_certificate_url_pointer_check RESTRICT;
+
+ALTER TABLE vouchers DROP CONSTRAINT vouchers_symbol_check RESTRICT;
+ALTER TABLE vouchers DROP CONSTRAINT vouchers_voucher_name_check RESTRICT;
+ALTER TABLE vouchers DROP CONSTRAINT vouchers_voucher_address_check RESTRICT;
+ALTER TABLE vouchers DROP CONSTRAINT vouchers_voucher_address_check1 RESTRICT;
+ALTER TABLE vouchers DROP CONSTRAINT vouchers_location_name_check RESTRICT;
+
+ALTER TABLE vpa DROP CONSTRAINT vpa_vpa_check RESTRICT;
