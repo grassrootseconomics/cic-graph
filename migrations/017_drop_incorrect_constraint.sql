a551cae1277f6c214146e279fa5afa0e5bd64497
William Luke <williamluke4@gmail.com>
2025-05-20 14:56:24 +0300

    fix: add migration service to docker-compose and update SQL migration to clean up swap_pool_tags

--- migrations/017_drop_incorrect_constraint.sql
+++ migrations/017_drop_incorrect_constraint.sql
@@ -1,5 +1,12 @@
 ALTER TABLE swap_pool_tags
 DROP CONSTRAINT IF EXISTS swap_pool_tags_swap_pool_fkey;
 
+DELETE FROM swap_pool_tags
+WHERE
+    swap_pool NOT IN(
+        SELECT id
+        FROM swap_pools
+    );
+
 ALTER TABLE swap_pool_tags
 ADD CONSTRAINT swap_pool_tags_swap_pool_fkey FOREIGN KEY (swap_pool) REFERENCES swap_pools (id);
